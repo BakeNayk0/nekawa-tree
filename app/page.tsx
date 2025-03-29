d675d53e5ea74c89c0d16dde24cec1a62ac05924
BakaNayk0 <cybernayk0@gmail.com>
2025-03-30 00:04:26 +0100

    base UX

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,130 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { cn } from "@/lib/utils";
+import { Instagram, Twitch } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className={cn("flex flex-col gap-12 font-sans")}>
+      <div className="flex items-center">
+        <div className="rounded-full p-2 w-fit">
+          <Image alt="" src="/nekawa_hero.png" width={60} height={60} />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <h1 className="text-4xl">Nekawa</h1>
+        <div className="flex items-center justify-between gap-8 ml-auto">
+          <Instagram size={60} />
+          <Twitch size={60} />
+          <Button size={"xl"}>Contact us</Button>
+        </div>
+      </div>
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-center justify-center bg-blue-950 w-full h-96 rounded-3xl">
+          <div className="z-10 items-center w-fit text-white text-[120px] mb-12">
+            Nekawa Tree
+          </div>
+        </div>
+        <div className="flex items-start gap-4">
+          <div className="flex flex-col gap-8 max-w-sm max-h-[150px]">
+            <p className="text-blue-900 text-4xl font-bold">
+              Découvre notre univers
+            </p>
+            <p className="text-blue-900 font-normal text-sm font-mono leading-8">
+              Un chat et une loutre un peu geek. Tous les deux passionnées par
+              l&apos;univers celtique et Japonais. Il n&apos;en fallait pas plus
+              pour partir à l&apos;aventure.
+            </p>
+            <Button size={"xl"}>Nous découvrir</Button>
+          </div>
+          <Card className="relative bg-emerald-200 w-1/4 h-72">
+            <CardContent className="my-auto mx-auto">
+              <Image alt="" src="/nekawa_hero.png" width={120} height={120} />
+            </CardContent>
+            <Button
+              className="text-xl absolute top-64 left-[15%] rounded-xl"
+              size={"lg"}
+            >
+              Webdesign
+            </Button>
+          </Card>
+          <Card className="relative bg-emerald-200 w-1/4 h-72">
+            <CardContent className="my-auto mx-auto">
+              <Image alt="" src="/nekawa_hero.png" width={120} height={120} />
+            </CardContent>
+            <Button
+              className="text-xl absolute top-64 left-[15%] rounded-xl"
+              size={"lg"}
+            >
+              Nayko.dev
+            </Button>
+          </Card>
+          <Card className="relative bg-emerald-200 w-1/4 h-72">
+            <CardContent className="my-auto mx-auto">
+              <Image alt="" src="/nekawa_hero.png" width={120} height={120} />
+            </CardContent>
+            <Button
+              className="text-xl absolute top-64 left-[15%] rounded-xl"
+              size={"lg"}
+            >
+              Shop
+            </Button>
+          </Card>
+          <Card className="relative bg-emerald-200 w-1/4 h-72">
+            <CardContent className="my-auto mx-auto">
+              <Image alt="" src="/nekawa_hero.png" width={120} height={120} />
+            </CardContent>
+            <Button
+              className="text-xl absolute top-64 left-[15%] rounded-xl"
+              size={"lg"}
+            >
+              Aquarelle
+            </Button>
+          </Card>
+        </div>
+      </div>
+      <Carousel>
+        <CarouselContent>
+          <CarouselItem>
+            <div className="flex w-full bg-teal-200/20">
+            <Card className="bg-emerald-200 w-1/4 h-72">
+              <CardContent className="my-auto mx-auto">
+                <Image alt="" src="/nekawa_hero.png" width={120} height={120} />
+              </CardContent>
+            </Card>
+            <p>Une description</p>
+            </div>
+          </CarouselItem>
+          <CarouselItem>
+            <div className="flex w-full bg-teal-200/20">
+            <Card className="bg-emerald-200 w-1/4 h-72">
+              <CardContent className="my-auto mx-auto">
+                <Image alt="" src="/nekawa_hero.png" width={120} height={120} />
+              </CardContent>
+            </Card>
+            <p>Une description</p>
+            </div>
+          </CarouselItem>
+          <CarouselItem>
+            <div className="flex w-full bg-teal-200/20">
+            <Card className="bg-emerald-200 w-1/4 h-72">
+              <CardContent className="my-auto mx-auto">
+                <Image alt="" src="/nekawa_hero.png" width={120} height={120} />
+              </CardContent>
+            </Card>
+            <p>Une description</p>
+            </div>
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 }
